408e6bd9365f0088a5f200c648cd5fa4cb0d8b0d
Julian Pasquale <julianagustin78@gmail.com>
2022-08-06 11:30:17 -0300

    Update mappings and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,27 +1,12 @@
-POSTS_COUNT = 1_000_000
-THREADS_COUNT = 10
+POSTS_COUNT = 1_000
 
-puts '========== Generating data =========='
-
-# Using 10 threads
-threads = Array.new(THREADS_COUNT) do |index|
-  Thread.new do
-    result = Array.new(POSTS_COUNT / THREADS_COUNT) do
-      {
-        title: Faker::Lorem.sentence, 
-        body: Faker::Lorem.paragraph,
-        topic: Faker::Company.industry
-      }
-    end
-
-    puts "Finished thread #{index}"
-
-    result
-  end
+posts_data = Array.new(POSTS_COUNT) do
+  {
+    title: Faker::Lorem.sentence,
+    body: Faker::Lorem.paragraph,
+    topic: Faker::Company.industry
+  }
 end
-
-posts_data = threads.flat_map(&:value)
-
 puts '========== Data generated =========='
 
 if Post.create(posts_data)
@@ -31,3 +16,9 @@ else
 end
 
 puts "========== Now you have #{Post.count} =========="
+
+puts "========== Importing to Elastic =========="
+Post.__elasticsearch__.create_index!
+Post.import
+
+puts "========== Done =========="
