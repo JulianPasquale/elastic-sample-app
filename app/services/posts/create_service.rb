d56c82325d84c180de0539bd752e8c94d8e6d542
Julian Pasquale <julianagustin78@gmail.com>
2023-02-03 10:37:50 -0300

    Fix namespace

--- app/services/posts/create_service.rb
+++ app/services/posts/create_service.rb
@@ -1,20 +1,18 @@
-module Elastic
-  module Posts
-    class CreateService
-      def initialize(post_params)
-        @params = post_params
-      end
-
-      def call
-        post = Post.create!(params)
-        ElasticIndexerJob.perform_later(:create, post)
-
-        post
-      end
-
-      private
-
-      attr_reader :params
+module Posts
+  class CreateService
+    def initialize(post_params)
+      @params = post_params
     end
+
+    def call
+      post = Post.create!(params)
+      ElasticIndexerJob.perform_later(:create, post)
+
+      post
+    end
+
+    private
+
+    attr_reader :params
   end
 end
